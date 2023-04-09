99a87564b23f61ab617c1a257c0f15d7679538d1
hyejin.shin <hyejin.shin@asurion.com>
2023-04-09 19:26:37 +0900

    init commit, setting ui

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import classes from  "./Header.module.css";
+import mealsImg from '../../assets/meals.jpg'
+import HeaderCartButton from "./HeaderCartButton";
+const Header = () => {
+  return ( 
+    <>
+      <header className={classes.header}>
+        <h1>음식 주문</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImg} alt="" />
+      </div>
+    </>
+  );
+};
+export default Header;
