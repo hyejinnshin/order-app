cf85a49e02a0fa86a3a0b422cb222ef02deb04c2
hyejin.shin <hyejin.shin@asurion.com>
2023-04-09 21:39:02 +0900

    use context, reducer

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,27 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `${cartCtx.totalAmount}원`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[
-        {
-          id: "c1",
-          name: "초밥",
-          amount: 2,
-          price: 20000,
-        },
-      ].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null,  item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        >
+          {item.name}
+        </CartItem>
       ))}
     </ul>
   );
@@ -20,11 +30,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>총 주문금액</span>
-        <span>30000</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>닫기</button>
-        <button className={classes.button}>주문하기</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          닫기
+        </button>
+        {hasItems && <button className={classes.button}>주문하기</button>}
       </div>
     </Modal>
   );
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,27 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredAmout = amountInputRef.current.value;
+    const enteredAmoutNumber = +enteredAmout;
+    if (
+      enteredAmout.trim().length === 0 ||
+      enteredAmoutNumber < 1 ||
+      enteredAmoutNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmoutNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="수량"
         input={{
           id: "amount_" + props.id,
@@ -15,6 +33,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>추가</button>
+      {!amountIsValid  && <p>주문할 음식의 수량을 선택해주세요</p>}
     </form>
   );
 };
